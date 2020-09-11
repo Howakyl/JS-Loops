7a3ac3377fe7769de794aa66809fcfc389c452b0
Howakyl96 <howakyl@gmail.com>
2020-09-10 20:58:34 -0700

    Commit 2 - Get Even answered

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,12 @@
 //Easy going 
 //1)
-for (let i = 1; i < 21; i++) {
+// for (let i = 1; i < 21; i++) {
+//     console.log(i);
+// }
+
+//Get Even
+// 1)
+
+for (let i =0; i < 201; i += 2) {
     console.log(i);
-}
+};
