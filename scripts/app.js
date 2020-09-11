b9c337c96f6b1a1569ab0bd78dae6b0247416f1e
Howakyl96 <howakyl@gmail.com>
2020-09-10 21:20:47 -0700

    Commit 3 - Excited Kittens answered

--- scripts/app.js
+++ scripts/app.js
@@ -4,9 +4,38 @@
 //     console.log(i);
 // }
 
+//////////////////////////////////////////
 //Get Even
 // 1)
 
-for (let i =0; i < 201; i += 2) {
-    console.log(i);
-};
+// for (let i =0; i < 201; i += 2) {
+//     console.log(i);
+// };
+
+//////////////////////////////////////////
+
+//Excited Kitten
+//1) 
+// for (i = 0; i < 20; i++) {
+//     console.log('Love me, pet me! HSSSSSS!');
+// }
+//2)
+const catResponses = ['...human...why you taking pictures of me?...',
+'...the catnip made me do it...',
+'...why does the red dot always get away...']
+
+for (i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+        let randomNum = Math.floor(Math.random() * 3);
+
+        if (randomNum === 0) {
+            console.log(catResponses[0]);
+        } else if (randomNum === 1) {
+            console.log(catResponses[1]);
+        } else if (randomNum === 2) {
+            console.log(catResponses[2]);
+        }
+    } else {
+        console.log('Love me, pet me! HSSSSSS!');
+    }
+}
