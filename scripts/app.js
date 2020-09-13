7216b220c02fef0b29e7ab5d4868ae9d4d93323b
Howakyl96 <howakyl@gmail.com>
2020-09-12 20:13:09 -0700

    Commit 5 - Getting to know you answered

--- scripts/app.js
+++ scripts/app.js
@@ -44,14 +44,49 @@
 
 //Fizz Buzz
 
-for (let i = 1; i < 101; i++) {
-    if (i % 15 === 0) {
-        console.log('FizzBuzz');
-    } else if (i % 5 === 0) {
-        console.log('Buzz');
-    } else if (i % 3 === 0) {
-        console.log('Fizz')
-    } else {
-        console.log(i);
-    }
-};
+// for (let i = 1; i < 101; i++) {
+//     if (i % 15 === 0) {
+//         console.log('FizzBuzz');
+//     } else if (i % 5 === 0) {
+//         console.log('Buzz');
+//     } else if (i % 3 === 0) {
+//         console.log('Fizz')
+//     } else {
+//         console.log(i);
+//     }
+// };
+
+//////////////////////////////////////////
+
+//Getting to Know You
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+//1)
+kenny.shift();
+kenny.unshift('Gameboy');
+console.log(kenny);
+
+//2)
+jimClark[1] += 1;
+console.log(jimClark);
+
+//3)
+ryan[2] = 'Gotham City';
+console.log(ryan);
+
+//4) 
+reuben.splice(2,1,'Chicago');
+console.log(reuben);
+
+//5)
+jimHaff.splice(2,3,'Seattle' , 'San Fransisco' , 'Portland');
+console.log(jimHaff);
+
+//6)
+jimHaff.splice(2, 1);
+console.log(jimHaff);
